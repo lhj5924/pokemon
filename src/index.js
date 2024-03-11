ab2e159fc77563edad10fb24ba5e97d5f1701f25
HyunJin, Lee <nagi5924@gmail.com>
2024-03-11 23:31:47 +0900

    feat: gh-pages

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <BrowserRouter basename={process.env.PUBLIC_URL}> 
   <Provider store={store}>
     <App />
   </Provider>
-  </>
+  </BrowserRouter>
 );
